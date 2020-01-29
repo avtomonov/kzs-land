b84b7bc4e8e1c018344792e165959edf3543c23c
Andrey Avtomonov <web@hu2.ru>
2020-01-29 19:02:17 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var BigSlider = new Swiper ('.back-slider', {
+    loop: true,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    speed: 1000,
+})
+
+
+
+
+var contentSlider = new Swiper ('.content-slider', {
+    loop: true,
+    speed: 1000,
+    thumbs: {
+        swiper: BigSlider,
+    },
+    pagination: {
+        el: '.big-slider-pagination',
+        type: 'bullets',
+        clickable: true
+    },
+    autoplay: {
+     delay: 10000,
+    },
+})
+
+
+ AOS.init();
+
+ document.querySelector('.form-kzs').addEventListener('submit', function(e) {
+    e.preventDefault();
+});
